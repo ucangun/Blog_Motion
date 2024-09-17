431e4a4e167d8660095ea16512cb3fe2bf920dca
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-17 10:21:16 +0200

    "Added useNavigate from react-router-dom to useAuthCall hook and navigate to /login after registration and to / after login"

--- src/hooks/useAuthCall.tsx
+++ src/hooks/useAuthCall.tsx
@@ -3,11 +3,13 @@ import { RegisterFormValues } from "../pages/Register";
 import axios from "axios";
 import { fetchFail, fetchStart, registerSuccess } from "../features/authSlice";
 import { LoginFormValues } from "../pages/Login";
+import { useNavigate } from "react-router-dom";
 
 const BASE_URL: string = import.meta.env.VITE_BASE_URL;
 
 const useAuthCall = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   // register
   const register = async (userInfo: RegisterFormValues): Promise<void> => {
@@ -16,6 +18,7 @@ const useAuthCall = () => {
       const { data } = await axios.post(`${BASE_URL}users`, userInfo);
       console.log(data);
       dispatch(registerSuccess(data));
+      navigate("/login");
     } catch (error) {
       dispatch(fetchFail());
       console.error(error);
@@ -28,6 +31,7 @@ const useAuthCall = () => {
       dispatch(fetchStart());
       const { data } = await axios.post(`${BASE_URL}auth/login`, userInfo);
       console.log(data);
+      navigate("/");
     } catch (error) {
       dispatch(fetchFail());
       console.error(error);
