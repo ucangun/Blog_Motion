b8f6e683940198bf095b6a36e627da6aaff211a4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-17 10:16:40 +0200

    "Added redux-persist to store, updated package.json and package-lock.json"

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,40 @@
+// https://redux-toolkit.js.org/tutorials/typescript
+
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 
-// https://redux-toolkit.js.org/tutorials/typescript
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
