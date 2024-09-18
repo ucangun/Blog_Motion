bb0e7f79251cddc1d1653a6dabaa4e23f65120cc
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-18 19:56:00 +0200

    "Moved toast notification to useEffect in PrivateRouter component"

--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -2,16 +2,28 @@ import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import { Navigate, Outlet } from "react-router-dom";
 import { toastError } from "../helpers/ToastNotify";
+import { useEffect } from "react";
+
+/*
+  This component protects private routes by checking if the user is authenticated.
+  If not, it shows a toast notification and redirects the user to the login page.
+  The useEffect ensures the toast is triggered outside of the render process.
+*/
 
 const PrivateRouter = () => {
   const { currentUser } = useSelector((state: RootState) => state.auth);
 
+  useEffect(() => {
+    if (!currentUser) {
+      toastError("You need to log in first!");
+    }
+  }, [currentUser]);
+
   if (!currentUser) {
-    toastError("You need to log in first!");
     return <Navigate to="/login" />;
-  } else {
-    return <Outlet />;
   }
+
+  return <Outlet />;
 };
 
 export default PrivateRouter;
