96882e5a72b7fb8ad2eb69de54c0a9f594c232b3
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-18 23:31:13 +0200

    "Added navigate function, Typography component, and MyButton component to Blog page, and updated layout with additional Box and Typography elements."

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useBlogCall from "../hooks/useBlogCall";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
+import MyButton from "../components/Button";
 
 const Blog = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { getSingleBlog } = useBlogCall();
   const { singleBlog } = useSelector((state: RootState) => state.blog);
@@ -36,6 +38,34 @@ const Blog = () => {
           alt={singleBlog?.title}
           style={{ borderRadius: ".5rem" }}
         />
+      </Box>
+
+      <Box
+        sx={{
+          padding: "1rem 3rem",
+        }}
+      >
+        <MyButton type="secondary" onClick={() => navigate(-1)}>
+          &larr; Back all Blogs
+        </MyButton>
+      </Box>
+
+      <Box
+        sx={{
+          padding: "2rem 3rem",
+        }}
+      >
+        <Box>
+          <Typography variant="h4">{singleBlog?.title}</Typography>
+        </Box>
+        <Typography
+          variant="body1"
+          sx={{
+            padding: "3rem 0",
+          }}
+        >
+          {singleBlog?.content}
+        </Typography>
       </Box>
     </Container>
   );
