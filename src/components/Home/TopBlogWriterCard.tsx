df994abc9d10c90be839164f59de3f31565a6596
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-21 21:04:17 +0200

    "Added navigation to TopBlogWriterCard component"

--- src/components/Home/TopBlogWriterCard.tsx
+++ src/components/Home/TopBlogWriterCard.tsx
@@ -1,26 +1,36 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 interface TopBlogCardType {
   blog: BlogPost;
   category: string;
 }
 
-const TopBlogWriterCard = ({ blog, category }: TopBlogCardType) => (
-  <Card sx={{ display: "flex", marginBottom: 2 }}>
-    <CardMedia
-      component="img"
-      sx={{ width: 151 }}
-      image={blog.image}
-      alt="image"
-    />
-    <CardContent>
-      <Typography variant="button">{category}</Typography>
-      <Typography variant="body2" color="text.secondary">
-        10 min read
-      </Typography>
-      <Typography variant="h6">{blog.title}</Typography>
-    </CardContent>
-  </Card>
-);
+const TopBlogWriterCard = ({ blog, category }: TopBlogCardType) => {
+  const navigate = useNavigate(); // Hook doğru yerde tanımlandı
+
+  return (
+    <Card
+      sx={{ display: "flex", marginBottom: 2, cursor: "pointer" }}
+      onClick={() => {
+        navigate(`/blog/${blog._id}`);
+      }}
+    >
+      <CardMedia
+        component="img"
+        sx={{ width: 151 }}
+        image={blog.image}
+        alt="image"
+      />
+      <CardContent>
+        <Typography variant="button">{category}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          10 min read
+        </Typography>
+        <Typography variant="h6">{blog.title}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default TopBlogWriterCard;
