b93d942f6aee2b6af66a9d8e1f21c02d1b855503
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-20 14:06:58 +0100

    `Remove VITE_LOCAL_BASE_URL from environment variable usage`

--- src/hooks/useAuthCall.tsx
+++ src/hooks/useAuthCall.tsx
@@ -18,8 +18,7 @@ import { SweetAlertIcons, SweetNotify } from "../helpers/SweetNotify";
 import { handleApiError } from "../helpers/handleApiError";
 import useAxios from "./useAxios";
 
-const BASE_URL: string =
-  import.meta.env.VITE_BASE_URL || import.meta.env.VITE_LOCAL_BASE_URL;
+const BASE_URL: string = import.meta.env.VITE_BASE_URL;
 
 const useAuthCall = () => {
   const dispatch = useDispatch();
