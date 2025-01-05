723eff74a217b15eb7105c4d05b573e16c0b6f70
ucangun <149247682+ucangun@users.noreply.github.com>
2025-01-05 10:30:09 +0100

    `Removed unused import and commented out code in BlogIcons.tsx`

--- src/components/Blogs/BlogIcons.tsx
+++ src/components/Blogs/BlogIcons.tsx
@@ -1,18 +1,18 @@
 import { Box, IconButton, Typography } from "@mui/material";
 import { CiRead } from "react-icons/ci";
 import { FaHeart, FaRegComments, FaRegHeart } from "react-icons/fa6";
-import useBlogCall from "../../hooks/useBlogCall";
+// import useBlogCall from "../../hooks/useBlogCall";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { BlogCardProps } from "./BlogCard";
 
 const BlogIcons = ({ item }: BlogCardProps) => {
-  const { addRemoveLike } = useBlogCall();
+  // const { addRemoveLike } = useBlogCall();
   const { currentUser } = useSelector((state: RootState) => state.auth);
 
-  const handleLikeClicked = () => {
-    addRemoveLike(item._id);
-  };
+  // const handleLikeClicked = () => {
+  //   addRemoveLike(item._id);
+  // };
 
   const iconStyle = {
     fontSize: "1.2rem",
@@ -33,7 +33,7 @@ const BlogIcons = ({ item }: BlogCardProps) => {
       }}
     >
       <Box sx={boxStyle}>
-        <IconButton onClick={handleLikeClicked}>
+        <IconButton>
           {item.likes.includes(currentUser ? currentUser?._id : "") ? (
             <FaHeart style={iconStyle} color="red" />
           ) : (
